5dfb54750d71d040ab0acf9a65c0ee7c7c769c12
Argenis Oldair Palacios Escobar A.K.A. Dr. Lfnt <Dr.Lfnt@gmail.com>
2014-11-03 17:38:12 -0600

    Archivos /*Sin Blog*/
    Disain.Co

---
+++ contacto/contacto_gracias.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if (IE 9)]><html class="no-js ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--> <html lang="es-MX"> <!--<![endif]-->
+<head>
+
+<!-- Meta Tags -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta charset="UTF-8">
+<meta name="google-site-verification" content="-oTDUtoJBNhEg1-4r3THwbeZyVfZbqfRDsjMwUIn3Zw" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta http-equiv="cleartype" content="on">
+
+<title>DisainCo | Contacto | Cristalizamos ideas | Agencia digital</title>   
+
+<meta content="Estudio de Diseño Integral" name="description">
+<meta content="Diseño, Diseno, SEO, Social Media, Diseño Web, Diseño Publicitario, Publicidad, Marketing, Diseño Gráfico" name="keywords">
+<meta content="Argenis Oldair Palacios A.K.A. Dr. Lfnt | Julio César Tinoco Marín" name="author">
+
+<!-- Especificaciones cel -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<meta name="HandheldFriendly" content="true"/>
+<meta name="MobileOptimized" content="320"/>   
+
+<!-- Mobile Internet Explorer ClearType Technology -->
+<!--[if IEMobile]>  <meta http-equiv="cleartype" content="on">  <![endif]-->
+
+<!-- Si Ud desea puede Eliminar el meta, PERO la funcion de éste es redireccionar a otra ruta despues de un tiempo de 8 sg ó los que desee -->
+<meta http-equiv="refresh" content="10;URL=http://disain.co/">
+
+<!-- TXT -->
+<link rel="author" type="text/plain" href="http://www.disain.co/humans.txt">
+
+<!-- Favicon -->
+<link rel="shortcut icon" type="image/x-icon" href="http://www.disain.co/img/favicon.ico" media="all">
+
+<!-- Menu -->
+<link rel="stylesheet" type="text/css" href="../css/nav.css" />
+
+<!-- Bootstrap -->
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+
+<!-- CSS General -->
+<link href="../css/main.css" rel="stylesheet">
+
+<!-- Supersized -->
+<link href="../css/supersized.css" rel="stylesheet">
+
+<!-- Font Icons -->
+<link rel="stylesheet" type="text/css" href="../css/font-awesome.css" />
+
+<!-- Shortcodes -->
+<link href="../css/shortcodes.css" rel="stylesheet">
+
+<!-- Responsive -->
+<link href="../css/bootstrap-responsive.min.css" rel="stylesheet">
+<link href="../css/responsive.css" rel="stylesheet">
+
+<!-- Fav Icon -->
+<link rel="shortcut icon" href="#">
+
+<link rel="apple-touch-icon" href="#">
+<link rel="apple-touch-icon" sizes="114x114" href="#">
+<link rel="apple-touch-icon" sizes="72x72" href="#">
+<link rel="apple-touch-icon" sizes="144x144" href="#">
+
+<!-- Modernizr -->
+<script src="../js/modernizr.js"></script>
+
+
+</head>
+
+<body>
+
+<!-- Sección Gracias -->
+<div class="page-alternate2">
+<div class="container">
+    <!-- Título de Página -->
+    <div class="row">
+        <div class="span12">
+            <div class="title-page">
+                <h2 class="title">Gracias</h2>
+                <h3 class="title-description">Su Mensaje ha sido enviado satisfactoriamente.</h3>
+                <h3 class="title-description">Nos comunicaremos con usted lo mas pronto posible.</h3>
+                <h3 class="title-description">Lo invitamos a seguir visitando nuestra Pagina Web.</h3>
+                <h3 class="title-description">Conozca las 
+                <a href="http://disain.co/diseno/" data-toggle="tooltip" title="Sección de Soluciones">Soluciones</a> y 
+                <a href="#" data-toggle="tooltip" title="No esperes más">Promociones</a> que ofrecemos 
+                para usted.</h3>
+            </div>
+        </div>
+    </div>
+    <!-- Fin Título de Página -->
+</div>
+</div>
+<!-- Fin Sección Gracias -->
+
+<!-- Footer -->
+<footer>
+	<p class="credits">Copyright 2013 Disainco. <b><a href="#" title="DisainCo | Cristalizamos Ideas">Aviso de 
+    Privacidad</a></b></p>
+    <ul>
+		<li><i class="fa fa-html5 fa-2x tec vsfade"></i></li>
+		<li><i class="fa fa-css3 fa-2x tec vsfade"></i></li>
+		<li><a href="http://www.disain.co/humans.txt" target="_blank">
+        <img src="http://humanstxt.org/img/oficial-logos/humanstxt-transparent-1ink.png" class="vsfade tec" alt="humans.txt">
+        </a></li>
+		<li><a href="http://www.iammobile.co.uk/results/www.disain.co">
+        <img src="http://www.iammobile.co.uk/images/iammobile-small.png" class="vsfade tec" width="45px" alt="I am Mobile">
+        </a></li>
+	</ul>
+</footer>
+<!-- End Footer -->
+
+<!-- Js -->
+<script src="../js/classie.js"></script><!-- Menu -->
+<script src="../js/gnmenu.js"></script><!-- Menu -->
+<script>new gnMenu( document.getElementById( 'gn-menu' ) );</script><!-- Menu -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> <!-- jQuery Core -->
+<script src="../js/bootstrap.min.js"></script> <!-- Bootstrap -->
+<script src="../js/supersized.3.2.7.min.js"></script> <!-- Slider -->
+<script src="../js/waypoints.js"></script> <!-- WayPoints -->
+<script src="../js/waypoints-sticky.js"></script> <!-- Waypoints for Header -->
+<script src="../js/plugins.js"></script> <!-- Contains: jPreloader, jQuery Easing, jQuery ScrollTo, jQuery One Page Navi -->
+<script src="../js/main.js"></script> <!-- Default JS -->
+<!-- Fin Js -->
+
+</body>
+</html>
---
+++ contacto/web_contacto.php
@@ -0,0 +1,73 @@
+<?php
+	include("smtpmail.php");
+
+	// Aqui colocamos los campos que tiene nuestro formulario
+
+	$nombre = $_POST['nombre'];
+	$email = $_POST['email'];
+	$telefono = $_POST['telefono'];
+	$asunto = $_POST['asunto'];
+	$comentario = $_POST['comentario'];
+	$error = '';
+	// Aqui comprobamos si el usuario ingreso los datos requeridos
+	if ($nombre == ""){ 
+		$error.="No ha ingresado su Nombre <BR>\n";
+	}if ($telefono == ""){ 
+		$error.="No ha ingresado su Teléfono <BR>\n";
+	}if ($email == ""){ 
+		$error.="No ha ingresado su Correo <BR>\n";
+   	}if(ereg("[a-z0-9_.]+@[a-z0-9]+[.][.a-z0-9]+",$email)==0 && $email!=""){
+		$error.="El Email ingresado no es valido <BR>\n";
+   	if ($asunto == ""){ 
+		$error.="No ha ingresado su Asunto <BR>\n";
+	}
+   	}if ($comentario==""){ 
+		$error.="No ha ingresado su Comentario <BR>\n";	
+   	}
+   	
+   	if ($error != ""){
+		// Este es el archivo que contendra el mensaje de error
+		include ("contacto_error.php");
+		exit;
+	}else{
+		
+		// Aqui armamos el mensaje
+
+		$TxtMensa="------------------------------------------------------\n\n";
+		$TxtMensa.="FORMULARIO CONTACTO DISAINCO \n\n";
+		$TxtMensa.="-----------------------------------------------------\n\n";
+		
+		
+		$TxtMensa.="NOMBRE: $nombre $apellido \n";
+		$TxtMensa.="\n\n";
+		$TxtMensa.="CORREO: $email\n";
+		$TxtMensa.="\n\n";
+		$TxtMensa.="TELÉFONO: $telefono\n";
+		$TxtMensa.="\n\n";
+		$TxtMensa.="MENSAJE: $comentario \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+
+	// Aqui hacemos el envio del email
+
+        $Mail =& new PHPMailer();
+        $Mail->IsSMTP();
+        $Mail->Host = "localhost:25";
+        $Mail->SMTPAuth = false;
+        $Mail->WordWrap = 50;
+        $Mail->FromName = $nombre;
+        $Mail->From = $email;
+        $Mail->Priority = 1;
+        $Mail->Subject = $asunto;
+        $Mail->Body = $TxtMensa;
+
+ 	 $Mail->AddBcc("contacto@disain.co");
+	 $Mail->AddBcc("a.palacios@disain.co");
+	 $Mail->AddBcc("c.marin@disain.co");
+	 $Mail->AddBcc("cesare.caesar.marin@gmail.com");
+	 $Mail->AddBcc("Dr.Lfnt@gmail.com");
+	 $Mail->Send();
+	// Este es el archivo que contendra el mensaje de agradecimiento o puede ingresar otra ruta para que lo redireccione despues de enviado el correo
+	 include ("contacto_gracias.php");
+	
+	}
+
+?>
