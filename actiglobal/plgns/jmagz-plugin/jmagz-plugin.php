98a80deea9be62d5c9251e8ef98ecc2159e306ce
Argenis Oldair Palacios Escobar A.K.A. Dr. Lfnt <Dr.Lfnt@gmail.com>
2016-05-24 17:11:38 -0500

    ok
    ok

---
+++ actiglobal/plgns/jmagz-plugin/jmagz-plugin.php
@@ -0,0 +1,57 @@
+<?php
+/*
+	Plugin Name: Jmagz Plugin
+	Plugin URI: http://jegtheme.com/
+	Description: Mandatory Plugin for Jmagz Themes
+	Version: 1.0.4
+	Author: Jegtheme
+	Author URI: http://jegtheme.com
+	License: GPL2
+*/
+
+defined( 'JMAGZ_PLUGIN_VERSION' ) 	    or define( 'JMAGZ_PLUGIN_VERSION', '1.0.5' );
+defined( 'JMAGZ_PLUGIN_URL' ) 		    or define( 'JMAGZ_PLUGIN_URL', plugins_url('jmagz-plugin'));
+defined( 'JMAGZ_PLUGIN_FILE' ) 		    or define( 'JMAGZ_PLUGIN_FILE',  __FILE__ );
+defined( 'JMAGZ_PLUGIN_DIR' ) 		    or define( 'JMAGZ_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+$jpostarray = array();
+$jreviewarray = array();
+
+function jeg_jmagz_plugin_load() {
+    if( defined('JEG_PLUGIN_VERSION') ) {
+        require_once JMAGZ_PLUGIN_DIR . '/lib/plugin-helper.php';
+        require_once JMAGZ_PLUGIN_DIR . '/lib/post-type.php';
+        require_once JMAGZ_PLUGIN_DIR . '/lib/shortcode.php';
+        require_once JMAGZ_PLUGIN_DIR . '/lib/build-shortcode.php';
+        require_once JMAGZ_PLUGIN_DIR . '/lib/filter-shortcode.php';
+        require_once JMAGZ_PLUGIN_DIR . '/lib/vc-integration.php';
+        require_once JMAGZ_PLUGIN_DIR . '/lib/additional-widget.php';
+        require_once JMAGZ_PLUGIN_DIR . '/lib/additional-filter.php';
+    }
+}
+
+add_action('plugins_loaded', 'jeg_jmagz_plugin_load');
+
+function jmagz_load_textdomain()
+{
+    $domain = 'jmagz-plugin';
+    $jeg_lang_dir = dirname( plugin_basename( JMAGZ_PLUGIN_FILE ) ) . '/lang/';
+
+    // Traditional WordPress plugin locale filter
+    $locale        = apply_filters( 'plugin_locale',  get_locale(), $domain );
+    $mofile        = sprintf( '%1$s-%2$s.mo', $domain, $locale );
+
+    // Setup paths to current locale file
+    $mofile_local  = $jeg_lang_dir . $mofile;
+    $mofile_global = WP_LANG_DIR . '/' . $domain . '/' . $mofile;
+
+
+    if ( file_exists( $mofile_global ) ) {
+        load_textdomain( $domain, $mofile_global );
+    } elseif ( file_exists( $mofile_local ) ) {
+        load_textdomain( $domain, $mofile_local );
+    } else {
+        load_plugin_textdomain( $domain, false, $jeg_lang_dir );
+    }
+}
+add_action('init', 'jmagz_load_textdomain');
---
+++ actiglobal/plgns/jmagz-plugin/readme.txt
@@ -0,0 +1,36 @@
+1.0.0
+- Initial Release
+
+1.0.1
+- Fix compatibility with older php version 5.3
+
+1.0.2
+- Fix compatibility with older php version 5.2
+
+1.0.3
+- fix side ads size
+- add option to have option on front page query
+- unique content group for landing page
+
+1.0.4
+- Translate some word that not yet translated
+- Fix ads background that close top heading background
+- Add additional ad type (shortcode)
+- Twitter share to use title as first word instead "Check this awesome article"
+- Update visual composer to version 4.4.3
+- Update WooCommerce compatibility to version 2.3.7
+- Fix HTML escape on WooCommerce
+- Fix Landing Slider bug on mobile
+- add VK share on single page
+- Fix ads size to use only horizontal ads on google ads
+- Fix mobile navigation that still using desktop logo
+- Fix PO location and name on jmagz-plugin, now can easily change using poedit or CodeStyling Localization
+
+1.0.5
+- fix related post wording that prevent YARPP plugin to work
+- add YARPP plugin into child themes so it can work also on child theme
+- Pull first image if none of attached image used
+- Update TGM Plugin class
+- Update WooCommerce compatibility to version 2.3.8
+- Update JPlugin to version 1.0.4 to improve Jmagz Importer
+- Add Review on RSS Feed
